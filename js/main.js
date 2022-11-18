46eabfa066299ca03362ba6af788bdc90cf4126e
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2022-11-18 15:45:06 +0530

    Upgrade to Next Version 0.1
    Following changes have been made in this commit -
    1. Add Sounds for button click and background
    2. Add all questions in Results Page
    3. Add Code for Invalid Input Prevention
    4. Other Functionality improvements
    5. Improve UI

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ const start_test_div = document.querySelector('.start_test_div')
 const start_now_btn = document.querySelector('.start_now_btn')
 
 
+
+
+
 // TEST PAGE NODES REFERENCES
 const test_title = document.querySelector('.test_title')
 const q_number = document.querySelector('.q_number')
@@ -31,6 +34,8 @@ const percent_obtained = document.querySelector('.percent_obtained')
 // const answer_from_user = document.querySelector('.answer_from_user').value
 // const quotient_from_user = document.querySelector('.quotient_from_user').value
 // const remainder_from_user =  document.querySelector('.remainder_from_user').value
+
+sound_player("background_music", "start", "loop", 0.5)
 
 
 const question_done_btn = document.querySelector('.question_done_btn')
@@ -97,24 +102,44 @@ function generateNumbers_and_sign(diff_lvl, sign) {
         arithmetic_sign = "/"
     }
 
+    // AVOIDING REPEATING PREVIOUS QUESTION's NUMBERS
+    for (const num in questions_array) {
+        if (upper_Number == num[0] && down_Number == num[1]) {
+            generateNumbers_and_sign(diff_lvl, sign)        
+        }
+    }
+
     return [upper_Number, down_Number, arithmetic_sign]
     
 }
 
 function getAnswer(){
+    sound_player("click_sound")
+
     let ans_of_user;
     if (q_type == 'division') {
         const quotient_from_user = document.querySelector('.quotient_from_user').value
         const remainder_from_user = document.querySelector('.remainder_from_user').value
         ans_of_user = [parseInt(quotient_from_user), parseInt(remainder_from_user)]
+        if (isNaN(ans_of_user[0]) || isNaN(ans_of_user[1])) {
+            return;            
+        }
+        if (ans_of_user[0] > 9999800001 || ans_of_user[1] > 9999800001) {
+            return;
+
+        }
     }
     else{
         ans_of_user = parseInt(document.querySelector('.answer_from_user').value)
+        if (isNaN(ans_of_user)) {
+            return;
+        }
+        if (ans_of_user > 9999800001) {
+            return;
+        }
     }
     let sign_of_question = sign.textContent;
-
     user_answers_array.push(ans_of_user);
-
     up_num = parseInt(upNumber.textContent)
     down_num = parseInt(downNumber.textContent)
     examiner(up_num, down_num, sign_of_question, ans_of_user)
@@ -165,22 +190,72 @@ function examiner(up_number, down_number, sign_of_question , answer_of_student){
     current_q_no = current_q_no+1
 
     if (current_q_no == amount_of_questions + 1) {
-        resultGenerator()        
+        resultGenerator()
     }
     else if (current_q_no == amount_of_questions){
         question_done_btn.style.backgroundColor = 'yellow'
         question_done_btn.textContent = "Let's Finish It!!"
     }
+    
     questionBoxGenerator()
 }
 
 function resultGenerator(){
+    sound_player("final_question", "stop")
+    sound_player("results_page_background_music", "start", "loop")
     test_page.style.display = 'none';
-    result_page.style.display = 'flex'
     total_marks.textContent = `${amount_of_questions}`
     marks_obtained.textContent = `${marks}`
     user_name.textContent = `${student_name}`
     percent_obtained.textContent = `${((marks / amount_of_questions)*100).toFixed(2)}`
+    
+    const all_questions_div = document.querySelector('.all_questions_div')
+
+    n = 1
+    while (n!=amount_of_questions+1) {
+        let q_num = n
+        let upnum = questions_array[n-1][0]
+        let downum = questions_array[n-1][1]
+        let s_of_q = sign.textContent
+        let user_answer_ = user_answers_array[n-1]
+        let real_answer_ = real_answers_array[n-1]
+        let right_or_wrong_; 
+        if (right_or_wrong_array[n-1]=="right") {
+            right_or_wrong_ = "is_correct"
+        }
+        else if (right_or_wrong_array[n - 1] == "wrong") {
+            right_or_wrong_ = "is_wrong"  
+        }
+
+        let question_code = `\n
+        <div class="question-serialno">
+            <span class="question_number_span">Question <span class="q_num">${q_num}</span></span>
+
+            <div class="q_numbers_div">
+                <div class="upNum_div">
+                    <span class="upNumber">${upnum}</span>
+                </div>
+                <div class="downNum_and_sign_div">
+                    <span class="sign">${s_of_q}</span>
+                    <span class="downNum">${downum}</span>
+                </div>
+            </div>
+            <div class="answers_div">
+                <div class="user_answer_div">
+                    <span class="user_answer ${right_or_wrong_}">${user_answer_}</span>
+                </div>
+                <div class="real_answer_div">
+                    <span class="real_answer">${real_answer_}</span>
+                </div>
+            </div>
+        </div>
+        `
+        all_questions_div.innerHTML = all_questions_div.innerHTML + question_code
+        n = n+1
+    }
+
+    result_page.style.display = 'flex'
+
 
 }
 
@@ -205,9 +280,40 @@ function questionBoxGenerator(){
     questions_array.push([up_num, down_num])
 }
 
+function sound_player(audio_name, start_or_stop="start", loop_or_noloops="noloop", volume=0.5){
+    let sound;
+    
+    sound = document.querySelector(`.${audio_name}`);
+    if (sound.duration<5){
+        if (sound.currentTime != 0) {
+            sound.currentTime = 0;
+        }
+    }
+    // else{
+    // }
+    
+    if (start_or_stop == "start") {
+        sound.play()
+        sound.play().catch(function (error) {console.log("Browser cannot play sound without user interaction first")});
+    }
+    else if (start_or_stop == "stop") {
+        sound.pause()
+
+    }
+    if (loop_or_noloops == "loop") {
+        sound.loop = true
+    }
+    else if (loop_or_noloops == "noloop") {
+        sound.loop = false
+    }
+
+    sound.volume = volume;
+    
+}
 
 
 function createTestpage(){
+    sound_player("background_music", "stop")
     start_test_div.remove();
     test_page.style.display = 'flex';
 
@@ -217,9 +323,9 @@ function createTestpage(){
     }
 
     test_title.textContent = `${q_type.toUpperCase()}`
-    if (isNaN(amount_of_questions)) {
-        amount_of_questions = 10;
-    }
+    // if (isNaN(amount_of_questions)) {
+    //     amount_of_questions = 10;
+    // }
     total_qs.textContent = `${amount_of_questions}`
     questionBoxGenerator()
 }
@@ -238,5 +344,11 @@ function getValues() {
     diff_lvl = document.querySelector('#difficulty').value;
     amount_of_questions = parseInt(document.querySelector('#no_of_ques').value);
     negative_marking = document.querySelector('#negmarking').checked;
+
+    if (student_name.length < 1 || isNaN(amount_of_questions)){
+        return;
+    }
+
+    sound_player("click_sound", "start")
     createStartornotpage()
 }
