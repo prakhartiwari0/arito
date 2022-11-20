ace9c9c74d4828c238c7da83afddb57e938b70a7
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2022-11-20 22:16:35 +0530

    Improve Test Page functionality
    This commit will make the input element automatically get
    focused on every new question, and it now
    supports enter key press to
    submit answer and generate new question

--- js/main.js
+++ js/main.js
@@ -287,7 +287,7 @@ function questionBoxGenerator(){
     else{
         document.querySelector('.answer_from_user').value = "";
     }
-    let q_stuff = generateNumbers_and_sign(diff_lvl ,q_type) 
+    let q_stuff = generateNumbers_and_sign(diff_lvl ,q_type)
     let up_num = q_stuff[0] 
     let down_num = q_stuff[1] 
     let num_sign = q_stuff[2] 
@@ -295,6 +295,29 @@ function questionBoxGenerator(){
     downNumber.textContent = `${down_num}`
     sign.textContent = `${num_sign}`
     q_number.textContent = `${current_q_no}`
+
+    // This part will focus the first input element in the page. It works both in (Add, Sub, Mul)[which has only one input element]
+    // and with Division which has two input elements.
+    const answer_input = document.querySelector('input')
+    answer_input.focus()
+
+    // This code will select all the input elements, and add the event listener to each of them,
+    // All the input elements are called because if we add event listener to only one input element, 
+    // then in the division questions's input elements, only the quotient input will be having this
+    // Event, and Remainder Input wouldn't have that, which is not a good thing.  
+    var answer_all_inputs = [].slice.call(document.querySelectorAll('input'));
+
+    answer_all_inputs.forEach(function (element, index) {
+        element.addEventListener("keypress", function (event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                question_done_btn.click();
+            }
+        });
+    });
     questions_array.push([up_num, down_num])
 }
 
